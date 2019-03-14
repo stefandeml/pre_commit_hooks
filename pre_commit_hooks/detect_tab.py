4a4cb1a75c5fa3cea4d7cff00317d0d7afd64fd7
sdeml <stefandeml@gmail.com>
2019-03-14 22:56:40 +0100

    first commit

---
+++ pre_commit_hooks/detect_tab.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+import argparse
+import sys
+from typing import Optional
+from typing import Sequence
+
+
+
+def main(argv=None):  # type: (Optional[Sequence[str]]) -> int
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to check')
+    args = parser.parse_args(argv)
+
+    tab_files = []
+
+    for filename in args.filenames:
+        with open(filename, 'rb') as f:
+            content = f.read()
+            if any(b'\t' in content):
+                tab_files.append(filename)
+
+    if tab_files: 
+        for tab_file in tab_files:
+            print('Tab found: {}'.format(tab_file))
+        return 0 # will only warn and not fail
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
